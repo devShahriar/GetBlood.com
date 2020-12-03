5c5e2ca336328be67313edf7ebe286d93d7f070e
Shahriar-shudip <shudip.shahriar@gmail.com>
2020-12-03 13:24:31 +0600

    a

--- backend/.history/controllers/BloodBankController_20201202235904.js
+++
@@ -1,8 +0,0 @@
-const db = require('../util/db');
-const blood_bank = require('../models/BloodBank')
-exports.getBlood=(req,res,next)=>{
-    const blood =req.body.blood_group;
-  //  blood_bank.getBlood(req , blood)
-   const result =blood_bank.getBlood(req , blood)
-   res.json({result:result})
-}
